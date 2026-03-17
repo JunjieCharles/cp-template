9b830bd9cbea61551d5811676e859f0786b20b8f
俊杰Charles <45029740+JunjieCharles@users.noreply.github.com>
2026-03-17 14:39:37 +0800

    Refactor SparseTable implementation and fix queries

--- Data Structure/Range Query/Sparse Table.cpp
+++ Data Structure/Range Query/Sparse Table.cpp
@@ -1,53 +1,54 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-template <typename T, typename Cmp = std::less<T>>
-struct SparseTable
-{
-    std::vector<std::vector<T>> v;
+template <typename T, typename Cmp = less<T>>
+struct SparseTable {
+    vector<vector<T>> v;
     Cmp cmp;
 
-    SparseTable(const std::vector<T> &a, Cmp cmp = Cmp()) : v(a.size()), cmp(cmp)
+    SparseTable(int n, Cmp cmp = Cmp()) : cmp(cmp)
+    {
+        int K = __lg(n);  // n cannot be 0
+        v.assign(K + 1, vector<T>(n));
+    }
+
+    void build(const vector<T> &a)
     {
         int n = a.size();
-        int K = std::bit_width(unsigned(n)) - 1;
-        for (int i = 0; i < n; i++)
-        {
-            v[i].resize(K + 1);
-            v[i][0] = a[i];
+        int K = __lg(n);
+        for (int i = 0; i < n; i++) {
+            v[0][i] = a[i];
         }
-        for (int k = 1; k <= K; k++)
-        {
-            for (int i = 0; i + (1 << k) - 1 < n; i++)
-            {
-                v[i][k] = std::min(v[i][k - 1], v[i + (1 << (k - 1))][k - 1], cmp);
+        for (int k = 1; k <= K; k++) {
+            for (int i = 0; i + (1 << k) - 1 < n; i++) {
+                v[k][i] = min(v[k - 1][i], v[k - 1][i + (1 << (k - 1))], cmp);
             }
         }
     }
 
     T query(int l, int r) const
     {
-        int k = std::bit_width(unsigned(r - l + 1)) - 1;
-        return std::min(v[l][k], v[r - (1 << k) + 1][k], cmp);
+        int k = __lg(r - l + 1);
+        return min(v[k][l], v[k][r - (1 << k) + 1], cmp);
     }
 };
 
-//https://www.luogu.com.cn/problem/P3865
+// https://www.luogu.com.cn/problem/P3865
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n, m;
-    std::cin >> n >> m;
-    std::vector<int> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        std::cin >> a[i];
+    cin >> n >> m;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
     }
-    SparseTable<int, std::greater<int>> st(a);
-    for (int i = 0; i < m; i++)
-    {
+    SparseTable<int, greater<int>> st(n + 1);
+    st.build(a);
+    for (int i = 0; i < m; i++) {
         int l, r;
-        std::cin >> l >> r;
-        std::cout << st.query(l - 1, r - 1) << '\n';
+        cin >> l >> r;
+        cout << st.query(l, r) << '\n';
     }
 }
