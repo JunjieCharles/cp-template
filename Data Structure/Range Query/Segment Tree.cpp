98aa4466bb6839614c793c6f6a2c32cdca834714
俊杰Charles <45029740+JunjieCharles@users.noreply.github.com>
2026-03-17 12:08:34 +0800

    Refactor Segment Tree implementation and update methods

--- Data Structure/Range Query/Segment Tree.cpp
+++ Data Structure/Range Query/Segment Tree.cpp
@@ -1,124 +1,120 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 template <typename T>
-struct SegmentTree
-{
-    struct Node
-    {
-        int l, r;
-        int lson = -1, rson = -1;
-        T v;
+struct SegmentTree {
+    struct Node {
+        T v = 0;
         T tmul = 1, tadd = 0;
-
-        Node(int i = 0, T x = 0) : l(i), r(i), v(x) {}
-        Node(int l, int r, T x) : l(l), r(r), v(x) {}
-
-        void update(T mul, T add)
+        
+        void apply(T mul, T add, int len)
         {
-            v = v * mul + add * (r - l + 1);
+            v = v * mul + add * len;
             tadd = tadd * mul + add;
             tmul = tmul * mul;
         }
     };
 
-    std::vector<Node> tr;
+    int n;
+    vector<Node> tr;
 
-    SegmentTree(int l, int r) : tr(1, Node(l, r, 0))
+    SegmentTree(int n) : n(n), tr((n + 1) << 2) {}
+
+    void push_up(int p)
     {
-        tr.reserve((r - l + 1) << 2);
+        tr[p].v = tr[p << 1].v + tr[p << 1 | 1].v;
     }
 
-    void push_down(int i)
+    void push_down(int p, int l, int r)
     {
-        if (tr[i].lson == -1)
-        {
-            int mid = (tr[i].l + tr[i].r) >> 1;
-            tr[i].lson = (int)tr.size();
-            tr.emplace_back(tr[i].l, mid, 0);
-            tr[i].rson = (int)tr.size();
-            tr.emplace_back(mid + 1, tr[i].r, 0);
-        }
-        tr[tr[i].lson].update(tr[i].tmul, tr[i].tadd);
-        tr[tr[i].rson].update(tr[i].tmul, tr[i].tadd);
-        tr[i].tadd = 0;
-        tr[i].tmul = 1;
+        int mid = (l + r) >> 1;
+        tr[p << 1].apply(tr[p].tmul, tr[p].tadd, mid - l + 1);
+        tr[p << 1 | 1].apply(tr[p].tmul, tr[p].tadd, r - mid);
+        tr[p].tmul = 1;
+        tr[p].tadd = 0;
     }
 
-    void update(int l, int r, T mul, T add, int i = 0)
+    void build(const vector<int>& a, int l, int r, int p = 1)
     {
-        if (tr[i].r < l || tr[i].l > r) return;
-        if (tr[i].l < tr[i].r) push_down(i);
-        if (l <= tr[i].l && r >= tr[i].r)
-        {
-            tr[i].update(mul, add);
+        if (l == r) {
+            tr[p].v = a[l];
             return;
         }
-        update(l, r, mul, add, tr[i].lson);
-        update(l, r, mul, add, tr[i].rson);
-        tr[i].v = tr[tr[i].lson].v + tr[tr[i].rson].v;
+        int mid = (l + r) >> 1;
+        build(a, l, mid, p << 1);
+        build(a, mid + 1, r, p << 1 | 1);
+        push_up(p);
     }
 
-    T query(int l, int r, int i = 0)
+    void update(int ql, int qr, T mul, T add, int l, int r, int p = 1)
     {
-        if (tr[i].r < l || tr[i].l > r) return 0;
-        if (tr[i].l < tr[i].r) push_down(i);
-        if (l <= tr[i].l && r >= tr[i].r) return tr[i].v;
-        return query(l, r, tr[i].lson) + query(l, r, tr[i].rson);
+        if (ql <= l && r <= qr) {
+            tr[p].apply(mul, add, r - l + 1);
+            return;
+        }
+        push_down(p, l, r);
+        int mid = (l + r) >> 1;
+        if (ql <= mid) update(ql, qr, mul, add, l, mid, p << 1);
+        if (qr > mid)  update(ql, qr, mul, add, mid + 1, r, p << 1 | 1);
+        push_up(p);
+    }
+
+    T query(int ql, int qr, int l, int r, int p = 1)
+    {
+        if (ql <= l && r <= qr) return tr[p].v;
+        push_down(p, l, r);
+        int mid = (l + r) >> 1;
+        T res = 0;
+        if (ql <= mid) res = res + query(ql, qr, l, mid, p << 1);
+        if (qr > mid)  res = res + query(ql, qr, mid + 1, r, p << 1 | 1);
+        return res;
     }
 };
 
 // https://www.luogu.com.cn/problem/P3373
 template <int MOD>
-struct ModInt
-{
+struct ModInt {
     int x;
-
-    ModInt(int x = 0) : x(x % MOD) {}
-
-    ModInt operator+(const ModInt &u) const { return ModInt((x + u.x) % MOD); }
-    ModInt operator-(const ModInt &u) const { return ModInt((x - u.x + MOD) % MOD); }
-    ModInt operator*(const ModInt &u) const { return ModInt(1ll * x * u.x % MOD); }
+    ModInt(int x = 0) : x(x) {}
+    ModInt operator+(const ModInt &u) const { return x + u.x >= MOD ? x + u.x - MOD : x + u.x; }
+    ModInt operator-(const ModInt &u) const { return x - u.x < 0 ? x - u.x + MOD : x - u.x; }
+    ModInt operator*(const ModInt &u) const { return int(1ll * x * u.x % MOD); }
 };
 
 template <int MOD>
 void run(int n, int q)
 {
-    SegmentTree<ModInt<MOD>> tr(1, n);
-    for (int i = 1; i <= n; i++)
-    {
-        int x;
-        std::cin >> x;
-        tr.update(i, i, 1, x);
+    SegmentTree<ModInt<MOD>> tr(n);
+    
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
     }
-    for (int i = 0; i < q; i++)
-    {
+    tr.build(a, 1, n);
+
+    for (int i = 0; i < q; i++) {
         int z, x, y;
-        std::cin >> z >> x >> y;
-        if (z == 1)
-        {
+        cin >> z >> x >> y;
+        if (z == 1) {
             int k;
-            std::cin >> k;
-            tr.update(x, y, k, 0);
-        }
-        else if (z == 2)
-        {
+            cin >> k;
+            tr.update(x, y, k, 0, 1, n);
+        } else if (z == 2) {
             int k;
-            std::cin >> k;
-            tr.update(x, y, 1, k);
-        }
-        else
-        {
-            std::cout << tr.query(x, y).x << '\n';
+            cin >> k;
+            tr.update(x, y, 1, k, 1, n);
+        } else {
+            cout << tr.query(x, y, 1, n).x << '\n';
         }
     }
 }
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n, q, m;
-    std::cin >> n >> q >> m;
+    cin >> n >> q >> m;
     if (m == 38) run<38>(n, q);
     else run<571373>(n, q);
 }
